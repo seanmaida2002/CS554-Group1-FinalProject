e7856667116ce17b84f6357c1661f947b7bd5c1a
Sean Maida <smaida2002@yahoo.com>
2024-12-04 19:46:53 -0500

    made change password form a popup window

--- react_client/src/components/Profile.jsx
+++ react_client/src/components/Profile.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import '../App.css';
 import SignOutButton from "./SignOut";
 import ChangePassword from "./ChangePassword";
-function Profile() {
+import Popup from 'reactjs-popup';
+// import 'reactjs-popup/dist/index.css';
 
+function Profile() {
+    const [buttonPopup, setButtonPopup] = useState(false);
     return (
         <div className="card">
-            <ChangePassword />
+            <button className="changePassword-button" onClick={() => setButtonPopup(true)}>Change Password</button>
+            <Popup open={buttonPopup} closeOnDocumentClick onClose={() => setButtonPopup(false)} modal >
+                {(close) => (
+                    <div className="popup-overlay">
+                        <div className="popup">
+                            <ChangePassword />
+                            <button className="button" onClick={close}>Close</button>
+                        </div>
+                    </div>
+                )}
+            </Popup>
             <SignOutButton />
         </div>
     )
