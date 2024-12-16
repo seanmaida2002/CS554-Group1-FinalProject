224614fb05c58209198b5ad7f1781b68e71da378
Sean Maida <smaida2002@yahoo.com>
2024-12-16 12:09:50 -0500

    edited file types allowed

--- react_client/src/components/RegisterSocialSignOn.jsx
+++ react_client/src/components/RegisterSocialSignOn.jsx
@@ -119,7 +119,7 @@ function RegisterSocialSignOn() {
             <form onSubmit={handleSignUp}>
                 <div className='register-profile-picture-wrapper'>
                     <label>Profile Picture:</label>
-                    <input type='file' accept='image/*' multiple={false} onChange={uploadFile} />
+                    <input type='file' accept='image/jpeg, image/png, image/jpg' multiple={false} onChange={uploadFile} />
                     <br />
                     <br />
                     <div className='register-profile-picture-container'>
